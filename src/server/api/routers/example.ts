37b6ac2a4ed7f88a1ed29bca2ecd2b4535e857bd
NekoChan <aaron74327@gmail.com>
2023-05-08 14:49:14 -0700

    demo

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -5,12 +5,11 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(async ({ input, ctx }) => {
+      await ctx.prisma.example.findMany();
+
       return {
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
 });
